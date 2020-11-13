fbb2b0bbddb49040c860eb5af0714f6a84566352
Ife Olaifa <ifeolaifa5@gmail.com>
2020-11-13 04:01:45 +0000

    added login/register page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,6 @@
 class ProductsController < ApplicationController
-
+  before_filter :authorize
   def index
-    puts ENV['USERNAME'] 
-    puts ENV['PASSWORD']
     @products = Product.all.order(created_at: :desc)
   end
 
