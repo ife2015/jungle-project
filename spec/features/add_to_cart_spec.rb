b8c3196427408e1ad0d6de70374c0ec5c9d75397
Ife Olaifa <ifeolaifa5@gmail.com>
2020-11-18 00:16:26 +0000

    testing-add-to-cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end 
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+
+    # DEBUG / VERIFY
+    first(".btn-primary").click
+    save_screenshot
+    expect(page).to have_content('My Cart (1)')
+    expect(page).to have_css 'article.product', count: 10
+  end
+end
