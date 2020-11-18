03426a4819af9d13dd231537c724637f2c43af3a
Ife Olaifa <ifeolaifa5@gmail.com>
2020-11-18 00:38:32 +0000

    added chang to cart details

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -23,6 +23,5 @@ RSpec.feature "AddToCarts", type: :feature, js: true do
     first(".btn-primary").click
     save_screenshot
     expect(page).to have_content('My Cart (1)')
-    expect(page).to have_css 'article.product', count: 10
   end
 end
