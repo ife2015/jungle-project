e1fd86a5245a0ff9c28f14a080ea1a2f5693bd93
Ife Olaifa <ifeolaifa5@gmail.com>
2020-11-18 01:43:27 +0000

    modified user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if user = User.authenticate_with_credentials(params[:email], params[:password])
+    if (user = User.authenticate_with_credentials(params[:email], params[:password]))
       # success logic, log them in
-      session[:user_id] = user.id
+      session[:user_id] = User.find_by_email(params[:email]).id
       redirect_to '/'
     else
       # failure, render login form
